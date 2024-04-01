55cf13ce7940bc1660b83104fdfabb4015973d54
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-04-01 02:08:59 +0200

    card fixed

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -9,27 +9,16 @@ export default {
 
 <template>
 
-    <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3 my-3">
+<div class="col-sm-12 col-md-6 col-lg-3 col-xl-3 my-3">
     <div class="card">
-        <!-- Usa :src per il binding dinamico dell'attributo src e :alt per l'alt -->
-        <!-- Nota l'uso della notazione punto per accedere alle proprietà dell'oggetto card -->
-        <img :src="'http://127.0.0.1:8000/storage/' + card.image"  class="card-img-top" :alt="card.title">
+        <img :src="'http://127.0.0.1:8000/storage/' + card.image" class="card-img-top" :alt="card.title">
         <div class="card-body">
-            <!-- Qui l'interpolazione con {{ }} è corretta perché è nel contenuto di un elemento -->
-            <h5 class="card-title">{{ card.description }}</h5>
-
+            <h5 class="card-title">{{ card.location }}</h5>
+            <p class="card-text">{{ card.title }}</p>
+            <a href="#" class="btn btn-primary">Per informazioni</a>
         </div>
-    </div> 
-    <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3 my-3">
-        <div class="card">
-            <img :src="'http://127.0.0.1:8000/storage/' + card.image" class="card-img-top" :alt="card.title">
-            <div class="card-body">
-                <h5 class="card-title">{{ card.location }}</h5>
-                <p class="card-text">{{card.description}}</p>
-                <a href="#" class="btn btn-primary">Maggiori informazioni</a>
-            </div>
-        </div>
-    </div>
+    </div>    
+</div>
 
 </template>
 
