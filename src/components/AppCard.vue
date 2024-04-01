b8068aebfb2b257a86d24db544c0e38fb55a6e81
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-04-01 02:05:23 +0200

    card migliorate

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -8,23 +8,33 @@ export default {
 </script>
 
 <template>
-    <div class="col-3 my-3">
-    <div class="card">
-        <!-- Usa :src per il binding dinamico dell'attributo src e :alt per l'alt -->
-        <!-- Nota l'uso della notazione punto per accedere alle proprietà dell'oggetto card -->
-        <img :src="'http://127.0.0.1:8000/storage/' + card.image"  class="card-img-top" :alt="card.title">
-        <div class="card-body">
-            <!-- Qui l'interpolazione con {{ }} è corretta perché è nel contenuto di un elemento -->
-            <h5 class="card-title">{{ card.description }}</h5>
+    <!-- <div class="col-3 my-3 cards-container'">
+        <div class="personal-content">
+
+            <div class="image-card-container">
+                <img :src="'http://127.0.0.1:8000/storage/' + card.image" class="card-img-top" :alt="card.title">
+            </div>
+            <div class="apartment-details">
+
+                <h2 class="card-title">{{ card.description }}</h2>
+            </div>
+
+            <div class="card-body">
+                <a href="#" class="card-link">Clicca qui per maggiori informazioni</a>
+            </div>
         </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Zona casa</li>
-        </ul>
-        <div class="card-body">
-            <a href="#" class="card-link">Clicca qui per maggiori informazioni</a>
+    </div> -->
+    <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3 my-3">
+        <div class="card">
+            <img :src="'http://127.0.0.1:8000/storage/' + card.image" class="card-img-top" :alt="card.title">
+            <div class="card-body">
+                <h5 class="card-title">{{ card.location }}</h5>
+                <p class="card-text">{{card.description}}</p>
+                <a href="#" class="btn btn-primary">Maggiori informazioni</a>
+            </div>
         </div>
     </div>
-</div>
+
 </template>
 
 <style lang="scss">
