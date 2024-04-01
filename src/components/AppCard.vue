12667787456bc9cd767fca2951303050552c8401
SquallStrifeX <squallstrifex@gmail.com>
2024-04-01 02:18:28 +0200

    fix  card

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -10,18 +10,25 @@ export default {
 <template>
 
 <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3 my-3">
-    <div class="card">
+    <div class="card h-100">
         <img :src="'http://127.0.0.1:8000/storage/' + card.image" class="card-img-top" :alt="card.title">
-        <div class="card-body">
-            <h5 class="card-title">{{ card.location }}</h5>
-            <p class="card-text">{{ card.title }}</p>
-            <a href="#" class="btn btn-primary">Per informazioni</a>
+        <div class="card-body d-flex flex-column">
+            <h5 class="card-title text-truncate">{{ card.location }}</h5>
+            <p class="card-text flex-grow-1">{{ card.title }}</p>
+            <a href="#" class="btn btn-primary mt-auto">Per informazioni</a>
         </div>
     </div>    
 </div>
+
 
 </template>
 
 <style lang="scss">
 /* Qui puoi aggiungere lo stile specifico per il tuo componente */
+.card-title.text-truncate {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
 </style>
